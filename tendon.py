15dd41d515efd32c2c009b111383676939f9c444
KeunheeCho <34701112+KeunheeCho@users.noreply.github.com>
2018-01-04 22:16:49 +0900

    add docstring

--- tendon.py
+++ tendon.py
@@ -12,8 +12,7 @@ import my.util
 
 
 def strand(e, cw, hw, Ap=138.7):
-    """
-    Calculate stress of a strand composed with 2 materials
+    """심선과 측선이 다른 재료로 이루어진 강연선의 응력을 계산합니다.
 
     : param e: strain vector
     : param cw: properties of core wire, {'fn': function_name, 'fa': [argument list of fn], 'D': diameter, 'nu': Poisson's ratio}
@@ -25,13 +24,13 @@ def strand(e, cw, hw, Ap=138.7):
     R1 = cw['D'] / 2
     R2 = hw['D'] / 2
     r2 = R1 + R2
-    A1 = np.pi * R1**2
-    A2 = np.pi * R2**2
+    A1 = np.pi * R1 ** 2
+    A2 = np.pi * R2 ** 2
     p2 = hw['p']
     nu1 = cw['nu']
     nu2 = hw['nu']
     alpha2 = np.arctan(p2 / (2 * np.pi * r2))
-    C1 = (r2 * np.tan(alpha2)**2 - nu1 * R1) / (r2 * np.tan(alpha2)**2 + r2 + nu2 * R2)
+    C1 = (r2 * np.tan(alpha2) ** 2 - nu1 * R1) / (r2 * np.tan(alpha2) ** 2 + r2 + nu2 * R2)
 
     s = (A1 * cw['fn'](e, *cw['fa']) + 6 * A2 * hw['fn'](C1 * e, *hw['fa']) * np.sin(alpha2)) / Ap
 
@@ -69,7 +68,7 @@ class Tendon:
             self._t_active = t[-1]
             self._t_passive = t[0]
         else:
-            self._t_active = (t[0], t[-1], )
+            self._t_active = (t[0], t[-1],)
             self._t_passive = (t[0] + t[-1]) / 2.0  # tPassive should be determined such that force of LHS = force of RHS
         # Profile
         self._spl_profile = interpolate.splprep([x, y, z], s=0, k=k, u=self.t)[0]
@@ -77,21 +76,21 @@ class Tendon:
         der1 = interpolate.splev(self.t, self._spl_profile, der=1)
         der2 = interpolate.splev(self.t, self._spl_profile, der=2)
         # Arc length
-        self._ds = np.sqrt(der1[0]**2 + der1[1]**2 + der1[2]**2)
+        self._ds = np.sqrt(der1[0] ** 2 + der1[1] ** 2 + der1[2] ** 2)
         self._spl_ds = interpolate.splrep(self.t, self._ds)
         # Angle change
-        dc = np.sqrt((der2[2] * der1[1] - der2[1] * der1[2])**2 + (der2[0] * der1[2] - der2[2] * der1[0])**2 + (der2[1] * der1[0] - der2[0] * der1[1])**2) / self._ds**2
+        dc = np.sqrt((der2[2] * der1[1] - der2[1] * der1[2]) ** 2 + (der2[0] * der1[2] - der2[2] * der1[0]) ** 2 + (der2[1] * der1[0] - der2[0] * der1[1]) ** 2) / self._ds ** 2
         self._spl_dc = interpolate.splrep(self.t, dc)
 
     def profile(self, u):
         return interpolate.splev(u, self._spl_profile, ext=1)
 
     def _integrate_spl_scalar(self, ui, spl):
-        if self.active == 'R':      # passive ---- active
+        if self.active == 'R':  # passive ---- active
             return -interpolate.splint(self._t_active, ui, spl)
-        elif self.active == 'L':    # active ---- passive
+        elif self.active == 'L':  # active ---- passive
             return interpolate.splint(self._t_active, ui, spl)
-        else:                       # active ---- active
+        else:  # active ---- active
             if ui <= self._t_passive:
                 return interpolate.splint(self._t_active[0], ui, spl)
             else:
@@ -124,7 +123,7 @@ class Tendon:
 
             def objfn(ti):
                 e = sEA - 2 * (self._work_jack(ti, Pj, mu, kp) - self.force_jack(ti, Pj, mu, kp) * self.arc_length(ti))
-                return e**2
+                return e ** 2
 
             def force_loss_right_active(tmin, tmax):
                 tset = optimize.minimize_scalar(objfn, bounds=(tmin, tmax), method='bounded').x
@@ -138,13 +137,13 @@ class Tendon:
                 fas = 2 * (self.force_jack(tas, Pj, mu, kp) - self.force_jack(tset, Pj, mu, kp))
                 return tset, tas, fas
 
-            if self.active == 'R':      # passive ---- active
+            if self.active == 'R':  # passive ---- active
                 self.set_limit, tas2, fas2 = force_loss_right_active(self._t_passive, self._t_active)
                 splas = interpolate.splrep(tas2, fas2, k=1)
-            elif self.active == 'L':    # active ---- passive
+            elif self.active == 'L':  # active ---- passive
                 self.set_limit, tas1, fas1 = force_loss_left_active(self._t_active, self._t_passive)
                 splas = interpolate.splrep(tas1, fas1, k=1)
-            else:                       # active ---- active
+            else:  # active ---- active
                 tset1, tas1, fas1 = force_loss_left_active(self._t_active[0], self._t_passive)
                 tset2, tas2, fas2 = force_loss_right_active(self._t_passive, self._t_active[1])
                 splas = interpolate.splrep(np.hstack((tas1, tas2)), np.hstack((fas1, fas2)), k=1)
@@ -287,12 +286,12 @@ class TendonAnalysis:
             if kstep[0] is not None:
                 smart_strains_inc_opt = self._jacking_tendon_force(self.ismarts, self.u, 'J') / (self.tendons[self.ismarts].Ep * self.tendons[self.ismarts].Ap)
                 e = (smart_strains_inc_opt - smart_strains_inc_exp[kstep[0]][self.ismarts]) / smart_strains_inc_exp[kstep[0]][self.ismarts]
-                sum_error2 += sum(e**2)
+                sum_error2 += sum(e ** 2)
 
             # 1st setting stage
             smart_strains_inc_opt = self._jacking_tendon_force(self.ismarts, self.u, 'S') / (self.tendons[self.ismarts].Ep * self.tendons[self.ismarts].Ap)
             e = (smart_strains_inc_opt - smart_strains_inc_exp[kstep[1]][self.ismarts]) / smart_strains_inc_exp[kstep[1]][self.ismarts]
-            sum_error2 += sum(e**2)
+            sum_error2 += sum(e ** 2)
 
             for i in ijacking:
                 self._ijacked.append(i)
@@ -304,21 +303,21 @@ class TendonAnalysis:
                         section_strain = self._solve_equilibriums_at_section(ijacking, j, self.Ec, 'J')[0]
                         smart_strains_inc_opt[j] = self.strain_at_point(self.tendons[self.ismarts].profile(self.u[j]), section_strain)
                     e = (smart_strains_inc_opt - smart_strains_inc_exp[kstep[0]][self.ismarts]) / smart_strains_inc_exp[kstep[0]][self.ismarts]
-                    sum_error2 += sum(e**2)
+                    sum_error2 += sum(e ** 2)
 
                 # setting stages
                 for j in range(self.nsections):
                     section_strain = self._solve_equilibriums_at_section(ijacking, j, self.Ec, 'S')[0]
                     smart_strains_inc_opt[j] = self.strain_at_point(self.tendons[self.ismarts].profile(self.u[j]), section_strain)
                 e = (smart_strains_inc_opt - smart_strains_inc_exp[kstep[1]][self.ismarts]) / smart_strains_inc_exp[kstep[1]][self.ismarts]
-                sum_error2 += sum(e**2)
+                sum_error2 += sum(e ** 2)
 
                 for i in ijacking:
                     self._ijacked.append(i)
 
             return np.sqrt(sum_error2)
 
-        result = optimize.minimize(objfn, my.util.x2r(x0, xb), method='L-BFGS-B', bounds=my.util.make_rb(xb), options={'gtol': 1e-7}, disp=True)
+        result = optimize.minimize(objfn, my.util.x2r(x0, xb), method='L-BFGS-B', bounds=my.util.make_rb(xb), options={'gtol': 1e-7})
         self.mu[:], self.kp[:], self.slip[:], self.Ec = my.util.r2x(result.x, xb)
 
         return self.mu, self.kp, self.slip, self.Ec
@@ -430,243 +429,6 @@ class TendonAnalysis:
         A = self._A_jacked_tendons_at_section(self.u[jsection], A)
         A = self._A_concrete_at_section(jsection, Ec, A)
         section_strain = np.linalg.solve(A, b)
-        return section_strain, A, b
-
-    def _update_jacking_tendon_strain(self, kstep, ijacking, stage):
-        self.tendon_strains_opt[kstep] = dict()
-        for i in ijacking:
-            self.tendon_strains_opt[kstep][i] = self._jacking_tendon_force(i, self.u, stage) / (self.tendons[i].Ep * self.tendons[i].Ap)
-
-    def _update_tendon_strain(self, kstep, ijacking, Ec, stage):
-        self.tendon_strains_opt[kstep] = dict()
-        for i in self._ijacked:
-            self.tendon_strains_opt[kstep][i] = np.zeros(self.nsections)
-        for j in range(self.nsections):
-            section_strain = self._solve_equilibriums_at_section(ijacking, j, Ec, stage)[0]
-            for i in self._ijacked:
-                self.tendon_strains_opt[kstep][i][j] = self.tendon_strains_opt[self._lastStep][i][j] + self.strain_at_point(self.tendons[i].profile(self.u[j]), section_strain)
-        for i in ijacking:
-            self.tendon_strains_opt[kstep][i] = self._jacking_tendon_force(i, self.u, stage) / (self.tendons[i].Ep * self.tendons[i].Ap)
-
-
-class TendonAnalysisTemp:
-
-    def __init__(self, tendon_strains_exp, tendons, Fj, u, Ac, Icz, Icy, Qcz=0.0, Qcy=0.0, Icyz=0.0, ismarts=0):
-        self.tendon_strains_exp = tendon_strains_exp
-        self.tendons = tendons
-        self.Fj = Fj
-        self.u = u
-        self.Ac = Ac
-        self.Icz = Icz
-        self.Icy = Icy
-        if Qcz == 0.0:
-            self.Qcz = np.zeros_like(Ac)
-        if Qcy == 0.0:
-            self.Qcy = np.zeros_like(Ac)
-        if Icyz == 0.0:
-            self.Icyz = np.zeros_like(Ac)
-        self.ismarts = ismarts
-        #
-        self.ntendons = len(tendons)
-        self.nsections = u.size
-        #
-        self.mu = np.zeros(self.ntendons)
-        self.kp = np.zeros(self.ntendons)
-        self.slip = np.zeros(self.ntendons)
-        self.Ec = 0.0
-        #
-        self.tendon_strains_opt = dict()  # self.tendon_strains_opts[kstep][i_tendon] = strain vector
-        self._ijacked = list()
-
-    def optimize(self, ksteps, ijackings, x0, xb):
-        """
-        ksteps: [('1p', '1s'), (None, '2s'), ('3p', '3s'), ], jacking stage is optional(=None), setting stage is mandatory
-        ijackings: order of jacking tendons, [(0, ), (1, ), (2, ), ] or [(0, ), (1, 2, ), ]
-        x0, xb: optimization variables, [mu, kp, slip, Ec]
-        """
-        smart_strains_inc_exp = dict()
-        kstep = ksteps[0]
-        if kstep[0] is not None:
-            smart_strains_inc_exp[kstep[0]] = dict()
-            smart_strains_inc_exp[kstep[0]][self.ismarts] = self.tendon_strains_exp[kstep[0]][self.ismarts]
-        smart_strains_inc_exp[kstep[1]] = dict()
-        smart_strains_inc_exp[kstep[1]][self.ismarts] = self.tendon_strains_exp[kstep[1]][self.ismarts]
-        prev_step = kstep[1]
-        for kstep in ksteps[1:]:
-            if kstep[0] is not None:
-                smart_strains_inc_exp[kstep[0]] = dict()
-                smart_strains_inc_exp[kstep[0]][self.ismarts] = self.tendon_strains_exp[kstep[0]][self.ismarts] - self.tendon_strains_exp[prev_step][self.ismarts]
-            smart_strains_inc_exp[kstep[1]] = dict()
-            smart_strains_inc_exp[kstep[1]][self.ismarts] = self.tendon_strains_exp[kstep[1]][self.ismarts] - self.tendon_strains_exp[prev_step][self.ismarts]
-            prev_step = kstep[1]
-
-        def objfn(r):
-            self.mu[:], self.kp[:], self.slip[:], self.Ec = my.util.r2x(r, xb)
-            sum_error2 = 0.0
-            self._ijacked = list()
-
-            kstep, ijacking = ksteps[0], ijackings[0]
-
-            # 1st jacking stage
-            if kstep[0] is not None:
-                smart_strains_inc_opt = self._jacking_tendon_force(self.ismarts, self.u, 'J') / (self.tendons[self.ismarts].Ep * self.tendons[self.ismarts].Ap)
-                e = (smart_strains_inc_opt - smart_strains_inc_exp[kstep[0]][self.ismarts]) / smart_strains_inc_exp[kstep[0]][self.ismarts]
-                sum_error2 += sum(e**2)
-
-            # 1st setting stage
-            smart_strains_inc_opt = self._jacking_tendon_force(self.ismarts, self.u, 'S') / (self.tendons[self.ismarts].Ep * self.tendons[self.ismarts].Ap)
-            e = (smart_strains_inc_opt - smart_strains_inc_exp[kstep[1]][self.ismarts]) / smart_strains_inc_exp[kstep[1]][self.ismarts]
-            sum_error2 += sum(e**2)
-
-            for i in ijacking:
-                self._ijacked.append(i)
-
-            for kstep, ijacking in zip(ksteps[1:], ijackings[1:]):
-                # jacking stages
-                if kstep[0] is not None:
-                    for j in range(self.nsections):
-                        section_strain = self._solve_equilibriums_at_section(ijacking, j, self.Ec, 'J')[0]
-                        smart_strains_inc_opt[j] = self.strain_at_point(self.tendons[self.ismarts].profile(self.u[j]), section_strain)
-                    e = (smart_strains_inc_opt - smart_strains_inc_exp[kstep[0]][self.ismarts]) / smart_strains_inc_exp[kstep[0]][self.ismarts]
-                    sum_error2 += sum(e**2)
-
-                # setting stages
-                for j in range(self.nsections):
-                    section_strain = self._solve_equilibriums_at_section(ijacking, j, self.Ec, 'S')[0]
-                    smart_strains_inc_opt[j] = self.strain_at_point(self.tendons[self.ismarts].profile(self.u[j]), section_strain)
-                e = (smart_strains_inc_opt - smart_strains_inc_exp[kstep[1]][self.ismarts]) / smart_strains_inc_exp[kstep[1]][self.ismarts]
-                sum_error2 += sum(e**2)
-
-                for i in ijacking:
-                    self._ijacked.append(i)
-
-            return np.sqrt(sum_error2)
-
-        # result = optimize.minimize(objfn, my.util.x2r(x0, xb), method='L-BFGS-B', bounds=my.util.make_rb(xb), options={'gtol': 1e-7}, disp=True)
-        # result = optimize.basinhopping(objfn, my.util.x2r(x0, xb), minimizer_kwargs={'method': 'L-BFGS-B', 'disp'=True})
-        result = optimize.differential_evolution(objfn, my.util.make_rb(xb), disp=True)
-        self.mu[:], self.kp[:], self.slip[:], self.Ec = my.util.r2x(result.x, xb)
-
-        return self.mu, self.kp, self.slip, self.Ec
-
-    def update(self, ksteps, ijackings):
-        self._ijacked = list()
-
-        kstep, ijacking = ksteps[0], ijackings[0]
-        if kstep[0] is not None:
-            self._update_jacking_tendon_strain(kstep[0], ijacking, 'J')
-        self._update_jacking_tendon_strain(kstep[1], ijacking, 'S')
-        for i in ijacking:
-            self._ijacked.append(i)
-        self._lastStep = kstep[1]
-
-        for kstep, ijacking in zip(ksteps[1:], ijackings[1:]):
-            if kstep[0] is not None:
-                self._update_tendon_strain(kstep[0], ijacking, self.Ec, 'J')
-            self._update_tendon_strain(kstep[1], ijacking, self.Ec, 'S')
-            for i in ijacking:
-                self._ijacked.append(i)
-            self._lastStep = kstep[1]
-
-        return self.tendon_strains_opt
-
-    def plot(self, var='Strain'):  # var = 'Strain' / 'Stress' / 'Force'
-        for kstep in self.tendon_strains_opt:
-            plt.figure()
-            plt.title('Step = {}'.format(kstep))
-            for i in self.tendon_strains_opt[kstep]:
-                if var == 'Strain':
-                    plt.plot(self.u, self.tendon_strains_exp[kstep][i], 'o-', label='Tendon #{:d}: Measured'.format(i))
-                    plt.plot(self.u, self.tendon_strains_opt[kstep][i], '.-', label='Tendon #{:d}: Optimized'.format(i))
-                elif var == 'Stress':
-                    plt.plot(self.u, self.tendon_strains_exp[kstep][i] * self.tendons[i].Ep, 'o-', label='Tendon #{:d}: Measured'.format(i))
-                    plt.plot(self.u, self.tendon_strains_opt[kstep][i] * self.tendons[i].Ep, '.-', label='Tendon #{:d}: Optimized'.format(i))
-                else:  # 'Force'
-                    plt.plot(self.u, self.tendon_strains_exp[kstep][i] * self.tendons[i].Ep * self.tendons[i].Ap, 'o-', label='Tendon #{:d}: Measured'.format(i))
-                    plt.plot(self.u, self.tendon_strains_opt[kstep][i] * self.tendons[i].Ep * self.tendons[i].Ap, '.-', label='Tendon #{:d}: Optimized'.format(i))
-            my.util.adjust_axis()
-            plt.xlabel('Location')
-            plt.ylabel(var)
-            plt.grid()
-            plt.legend()
-            plt.tight_layout()
-            plt.show()
-
-    @staticmethod
-    def strain_at_point(xyz, section_strain):
-        return section_strain[0] - xyz[1] * section_strain[1] + xyz[2] * section_strain[2]
-
-    def _jacking_tendon_force(self, i, usections, stage):  # stage='J'acking / 'S'etting
-        if stage.upper() == 'J':
-            return self.tendons[i].force_jack(usections, self.Fj[i], self.mu[i], self.kp[i])
-        else:
-            return self.tendons[i].force_set(usections, self.Fj[i], self.mu[i], self.kp[i], self.slip[i])
-
-    def _b_jacking_tendons_at_section(self, ijacking, usection, stage, b=None):
-        if b is None:
-            b = np.zeros(3)
-        for i in ijacking:
-            tf = self._jacking_tendon_force(i, usection, stage)
-            b[0] -= tf
-            b[1] -= tf * self.tendons[i].profile(usection)[1]
-            b[2] -= tf * self.tendons[i].profile(usection)[2]
-        return b
-
-    def _A_jacked_tendons_at_section(self, usection, A=None):
-        if A is None:
-            A = np.zeros((3, 3))
-        for i in self._ijacked:
-            xyz = self.tendons[i].profile(usection)
-            tf = self.tendons[i].Ep * self.tendons[i].Ap
-            A[0, 0] += tf
-            A[1, 0] += tf * xyz[1]
-            A[2, 0] += tf * xyz[2]
-            #
-            tf1 = tf * xyz[1]
-            A[0, 1] -= tf1
-            A[1, 1] -= tf1 * xyz[1]
-            A[2, 1] -= tf1 * xyz[2]
-            #
-            tf2 = tf * xyz[2]
-            A[0, 2] += tf2
-            A[1, 2] += tf2 * xyz[1]
-            A[2, 2] += tf2 * xyz[2]
-        return A
-
-    def _A_concrete_at_section(self, jsection, Ec, A=None):
-        if A is None:
-            A = np.zeros((3, 3))
-        A[0, 0] += Ec * self.Ac[jsection]
-        A[1, 0] += Ec * self.Qcz[jsection]
-        A[2, 0] += Ec * self.Qcy[jsection]
-        #
-        A[0, 1] -= Ec * self.Qcz[jsection]
-        A[1, 1] -= Ec * self.Icz[jsection]
-        A[2, 1] -= Ec * self.Icyz[jsection]
-        #
-        A[0, 2] += Ec * self.Qcy[jsection]
-        A[1, 2] += Ec * self.Icyz[jsection]
-        A[2, 2] += Ec * self.Icy[jsection]
-        return A
-
-    def _solve_equilibriums_at_section(self, ijacking, jsection, Ec, stage):
-        A = np.zeros((3, 3))
-        b = np.zeros((3))
-        b = self._b_jacking_tendons_at_section(ijacking, self.u[jsection], stage, b)
-        A = self._A_jacked_tendons_at_section(self.u[jsection], A)
-        A = self._A_concrete_at_section(jsection, Ec, A)
-
-        section_strain = np.linalg.solve(A, b)  # following codes: Temporary codes <-----------------------------------------------------------------------------------------------
-        # A00 = A[0, 0]
-        # b0 = b[0]
-        # x0 = b0 / A00
-        # A = np.zeros((3, 3))
-        # b = np.zeros((3))
-        # section_strain = np.zeros(3)
-        # A[0, 0] = A00
-        # b[0] = b0
-        # section_strain[0] = x0
-
         return section_strain, A, b
 
     def _update_jacking_tendon_strain(self, kstep, ijacking, stage):
